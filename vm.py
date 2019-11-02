7ce537f81aa6b1da9eb52aa71f200f89d8fb3dbd
Thomas Ballinger <thomasballinger@gmail.com>
2019-11-01 20:02:58 -0700

    16m15s

--- vm.py
+++ vm.py
@@ -1,8 +1,14 @@
 import struct
 
+class Function:
+    def __init__(self, nparams, returns, code):
+        self.nparams = nparams
+        self.returns = returns
+        self.code = code
 
 class Machine:
-    def __init__(self, memsize=65536):
+    def __init__(self, functions, memsize=65536):
+        self.functions = functions
         self.stack = []
         self.memory = bytearray(memsize)
 
@@ -18,7 +24,13 @@ class Machine:
     def pop(self):
         return self.stack.pop()
 
-    def execute(self, instructions):
+    def call(self, func, *args):
+        locals = dict(enumerate(args))
+        self.execute(func.code, locals)
+        if func.returns:
+            return self.pop()
+
+    def execute(self, instructions, locals):
         for op, *args in instructions:
             print(op, args, self.stack)
             if op == 'const':
@@ -38,11 +50,34 @@ class Machine:
                 val = self.pop()
                 addr = self.pop()
                 self.store(addr, val)
+            elif op == 'local.get':
+                self.push(locals[args[0]])
+            elif op == 'local.set':
+                locals[args[0]] = self.pop()
+            elif op == 'call':
+                func = self.functions[args[0]]
+                fargs = reversed([self.pop() for _ in range(func.nparams)])
+                result = self.call(func, *fargs)
+                if func.returns:
+                    self.push(result)
             else:
                 raise RuntimeError(f'Bad op! {op}')
 
 
 def example():
+    # def update_position(x, v, dt):
+    #     return x + v*dt
+    #
+    update_position = Function(nparams=3, returns=True, code=[
+        ('local.get', 0), # x
+        ('local.get', 1), # v
+        ('local.get', 2), # dt
+        ('mul',),
+        ('add',),
+    ])
+
+    functions = [update_position]
+
     x_addr = 22
     v_addr = 42
 
@@ -53,15 +88,14 @@ def example():
         ('const', v_addr),
         ('load',),
         ('const', 0.1),
-        ('mul',),
-        ('add',),
+        ('call', 0),
         ('store',),
     ]
 
-    m = Machine()
+    m = Machine(functions)
     m.store(x_addr, 2.0)
     m.store(v_addr, 3.0)
-    m.execute(code)
+    m.execute(code, None)
     print("Result: ", m.load(x_addr));
 
 if __name__ == '__main__':
