9e8dd12f945c1a7a167c2b74155b54c329da13d1
Thomas Ballinger <thomasballinger@gmail.com>
2019-11-10 19:55:16 -0800

    9m45s

--- vm.py
+++ vm.py
@@ -1,6 +1,16 @@
+import struct
+
+
 class Machine:
-    def __init__(self):
+    def __init__(self, memsize=65536):
         self.stack = []
+        self.memory = bytearray(memsize)
+
+    def load(self, addr):
+        return struct.unpack('<d', self.memory[addr:addr+8])[0]
+
+    def store(self, addr, val):
+        self.memory[addr:addr+8] = struct.pack('<d', val)
 
     def push(self, item):
         self.stack.append(item)
@@ -11,34 +21,48 @@ class Machine:
     def execute(self, instructions):
         for op, *args in instructions:
             print(op, args, self.stack)
-            if op == "const":
+            if op == 'const':
                 self.push(args[0])
-            elif op == "add":
+            elif op == 'add':
                 right = self.pop()
                 left = self.pop()
                 self.push(left + right)
-            elif op == "mul":
+            elif op == 'mul':
                 right = self.pop()
                 left = self.pop()
                 self.push(left * right)
+            elif op == 'load':
+                addr = self.pop()
+                self.push(self.load(addr))
+            elif op == 'store':
+                val = self.pop()
+                addr = self.pop()
+                self.store(addr, val)
             else:
-                raise RuntimeError(f"Bad op! {op}")
+                raise RuntimeError(f'Bad op! {op}')
 
 
 def example():
-    # compute 2 + 3 * 0.1
+    x_addr = 22
+    v_addr = 42
+
     code = [
-        ("const", 2),
-        ("const", 3),
-        ("const", 0.1),
-        ("mul",),
-        ("add",),
+        ('const', x_addr),
+        ('const', x_addr),
+        ('load',),
+        ('const', v_addr),
+        ('load',),
+        ('const', 0.1),
+        ('mul',),
+        ('add',),
+        ('store',),
     ]
 
     m = Machine()
+    m.store(x_addr, 2.0)
+    m.store(v_addr, 3.0)
     m.execute(code)
-    print("Result: ", m.pop())
+    print("Result: ", m.load(x_addr));
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     example()
